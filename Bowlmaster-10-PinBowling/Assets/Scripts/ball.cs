1a14102f744a334c8b5a7924e468df772798f8b0
FBSALOMON <fbsalomon@gmail.com>
2018-07-20 00:19:01 -0400

    Bowlmaster: 10-Pin Bowling(11/58)

---
+++ Bowlmaster-10-PinBowling/Assets/Scripts/ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    public float launchSpeed;
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rigidbody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
