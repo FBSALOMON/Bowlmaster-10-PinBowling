0e94bce3e4287744638f0a8513fb7a8241a647bd
FBSALOMON <fbsalomon@gmail.com>
2018-07-22 23:47:26 -0400

    Bowlmaster: 10-Pin Bowling(20/58)

---
+++ Bowlmaster-10-PinBowling/Assets/Scripts/Pin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float standingThreshold = 3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(name + " " + IsStanding());
+	}
+
+    public bool IsStanding()
+    {
+        Vector3 rotationInEuller = transform.rotation.eulerAngles;
+        float tiltInX = Mathf.Abs(rotationInEuller.x);
+        float tiltInZ = Mathf.Abs(rotationInEuller.z);
+
+        if (tiltInX < standingThreshold && tiltInZ < standingThreshold)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Awake()
+    {
+        this.GetComponent<Rigidbody>().solverVelocityIterations = 10;
+    }
+
+}
